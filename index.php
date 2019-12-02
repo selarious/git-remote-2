d4e60b7bd834e071c1570a27dc5be342e7665087
selarious <ndhoee@gmail.com>
2019-12-02 17:03:45 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Git Remote</title>
+</head>
+<body>
+    <h3>Hello World</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quaerat doloremque eligendi, beatae nisi enim vitae impedit dolorum doloribus voluptatem neque distinctio, facere, rem officia deleniti sit quidem veniam perferendis!
+</p>
+    
+</body>
+</html>
